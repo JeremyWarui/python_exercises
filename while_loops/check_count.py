744e52f3fdce35650ad338582160b0234b37faab
JeremyWarui <jerywarne7@gmail.com>
2025-09-09 19:17:03 +0300

    Count by check the count step

--- while_loops/check_count.py
+++ while_loops/check_count.py
@@ -27,7 +27,7 @@ if start_num > end_num:
     result = "Oops! Looks like your start value is greater than the end value."
 else:
     break_num = start_num
-    while start_num <= end_num:
+    while break_num <= end_num:
         break_num += count_by
         result = break_num
 
