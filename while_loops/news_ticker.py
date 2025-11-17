037820984abd13065d00beae13ed9a5167f91b18
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2025-11-17 14:34:59 +0000

    News ticker

--- while_loops/news_ticker.py
+++ while_loops/news_ticker.py
@@ -9,14 +9,9 @@ headlines = ["Local Bear Eaten by Man",
 news_ticker = ""
 # write your loop here
 for word in headlines:
-    if len(news_ticker) + len(word) + 1 > 140:
-        remaining_space = 140 - len(news_ticker) - 1
-        if remaining_space > 0:
-            news_ticker += " " + word[:remaining_space]
+    news_ticker += word + " "
+    if len(news_ticker) >= 140:
+        news_ticker = news_ticker[:140]
         break
-    else:
-        if news_ticker:
-            news_ticker += " "
-        news_ticker += word 
 
 print(news_ticker)
